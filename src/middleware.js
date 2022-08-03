99842ad4111e333cdfc0e454801a61e3a66ae26d
Ryan de Menezes <menezesryan1010@gmail.com>
2022-08-03 15:01:00 -0300

    Arrumando o jwt

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,18 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = {
     authentication: (req, res, next) => {
-        next();
+        try {
+            const [, token] = req.headers.authorization.split(' ');
+        
+            const { restauranteId } = jwt.verify(token, process.env.SECRET);
+            req.headers.id = restauranteId;
+            next();
+        } catch (err){
+            res.status(103).json({
+                error: true,
+                message: "Token inválido",
+            });
+        }
     },
 };
